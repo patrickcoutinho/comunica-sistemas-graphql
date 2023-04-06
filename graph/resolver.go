b5fcee80294715e3dc55430ac4662ae5de9153a0
Patrick Coutinho <patrick@istyle.com.br>
2023-04-06 17:43:14 -0300

    curso fullcycle graphql

---
+++ graph/resolver.go
@@ -0,0 +1,13 @@
+package graph
+
+import (
+	"github.com/patrickcoutinho/fullcycle-graphql/internal/database"
+)
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	CategoryDB *database.Category
+}
